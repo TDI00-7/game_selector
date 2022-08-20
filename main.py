3a0d48b4ee04345fbcac58035bedf71640ff4c03
Tyler Drew <tylerdrew93@gmail.com>
2022-08-20 18:40:00 -0400

    made the game selectable and also asked user to replay after game finishes

--- main.py
+++ main.py
@@ -3,9 +3,7 @@ import ttt
 
 def main():
     game_on = True
-
     while game_on:
-   
         game_selection = input(f"What game would you like to play?\n(1) Rock Paper Scissors\n(2) Tic Tac Toe\n(3) I dont want to play anymore\n")
         if game_selection == "1":
             rps.play_rock_paper_sissors()
@@ -25,6 +23,7 @@ def play_again():
     if anwser == "n":
         game_on = False
         return game_on   
+
 
 if __name__ == "__main__":
     main()
